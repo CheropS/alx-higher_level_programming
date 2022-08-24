b53f5db53f3d100ceea863af761ca373de27f55f
CheropS <sh.cherop@gmail.com>
2022-08-24 12:25:45 +0000

    removed whitespace

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -3,6 +3,6 @@
 """prints the alphabet in lower case, not followed by a new line,\
         except q and e"""
 
-for letter in range (97, 123):
+for letter in range(97, 123):
     if chr(letter) != 'q' and chr(letter) != 'e':
         print("{}".format(chr(letter)), end="")
