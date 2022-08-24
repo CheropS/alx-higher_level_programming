efe8f209e99077dfe8eb3b5d6d25d03d98094387
CheropS <sh.cherop@gmail.com>
2022-08-24 09:25:15 +0000

    prints the alphabet except q and e

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""prints the alphabet in lower case, not followed by a new line,\
+        except q and e"""
+
+for letter in range (97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
