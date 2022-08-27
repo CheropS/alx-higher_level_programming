1b8900e8b488f35158a91c693b25646f45fb6702
CheropS <sh.cherop@gmail.com>
2022-08-27 12:17:40 +0000

    fix bugs

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
-"""print the alphabet in reverse order"""
-
 j = 0
-for c in range(ord('z'), ord('a') -1, -1):
+for c in range(ord('z'), ord('a') - 1, -1):
     print("{}".format(chr(c - 1)), end="")
     j = 32 if j == 0 else 0
---
+++ 0x01-python-if_else_loops_functions/linked_lists.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_listint - prints all elements of a listint_t list
+ * @h: pointer to head of list
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *current;
+	unsigned int n; /* number of nodes */
+
+	current = h;
+	n = 0;
+	while (current != NULL)
+	{
+		printf("%i\n", current->n);
+		current = current->next;
+		n++;
+	}
+	return (n);
+}
+
+/**
+ * add_nodeint_end - adds a new node at the end of a listint_t list
+ * @head: pointer to pointer of first node of listint_t list
+ * @n: integer to be included in new node
+ * Return: address of the new element or NULL if it fails
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new;
+	listint_t *current;
+
+	current = *head;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		while (current->next != NULL)
+			current = current->next;
+		current->next = new;
+	}
+
+	return (new);
+}
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: pointer to list to be freed
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
+}
+
+/**
+ * insert_node - function that inserts node at given index
+ * @idx: index of the list
+ * @n: interger for the new node
+ * Return: if the function fails - NULL else new node
+ */
+listint_t *insert_node(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new, *cpy = *head;
+	unsigned int node;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+
+	if (idx == 0)
+	{
+		new->next = cpy;
+		*head = new;
+		return (new);
+	}
+
+	for (node = 0; node < (idx - 1); node++)
+	{
+		if (cpy == NULL || cpy->next == NULL)
+			return (NULL);
+
+		cpy = cpy->next;
+	}
+
+	new->next = cpy->next;
+	cpy->next = new;
+
+	return (new);
+}
