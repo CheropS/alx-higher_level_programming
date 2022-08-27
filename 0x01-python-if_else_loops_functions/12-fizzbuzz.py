05fc0fd8aa89dcd3f6938026c86c71137551c1e6
CheropS <sh.cherop@gmail.com>
2022-08-27 10:52:57 +0000

    add space after fizz, buzz and fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
 
-"""prints fizzbuzz if it is not a multiple of 3 or 5\
-        prints fizz for multiples of 3\
-        prints buzz for mulltiples of 5."""
-
 def fizzbuzz():
 
 
-    for number in range(0,101):
+    for number in range(0, 101):
         if number % 3 == 0 and number % 5 == 0:
-            print("FizzBuzz", end="")
+            print("FizzBuzz ", end="")
         elif number % 3 == 0:
-            print("Fizz", end="")
+            print("Fizz ", end="")
         elif number % 5 == 0:
-            print("Buzz", end="")
+            print("Buzz ", end="")
         else:
             print("{} ".format(number), end="")
-
