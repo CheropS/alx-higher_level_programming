2a83350b2728482470ffac267db66a4fc33c69c3
CheropS <sh.cherop@gmail.com>
2022-08-31 20:23:55 +0000

    search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,6 +3,6 @@
 def search_replace(my_list, search, replace):
     search_list = my_list[:]
     for i in range(len(search_list)):
-        if search_list[i] == search
+        if search_list[i] == search:
             search_list[i] = replace
     return (search_list)
