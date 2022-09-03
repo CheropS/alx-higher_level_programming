88211691a5ddbbf98eb7d3f83fb8da22c3f985cb
CheropS <sh.cherop@gmail.com>
2022-09-03 08:29:24 +0000

    errors fix

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,14 +2,10 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
 
-    if len(tuple_a) < 2:
-        if len(tuple_a) == 0:
-            tuple_a = 0, 0
-        else:
-            tuple_a = tuple_a[0], 0
-        if len(tuple_b) < 2:
-            if len(tuple_b) == 0:
-                tuple_b = 0, 0
-            else:
-                tuple_b = tuple_b[0], 0
-        return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    a = len(tuple_a)
+    b = len(tuple_b)
+
+    summation = ((tuple_a[0] if a > 0 else 0) + (tuple_b[0] if b > 0 else 0),
+                (tuple_a[1] if a > 1 else 0) + (tuple_b[1] if b > 1 else 0))
+
+    return summation
