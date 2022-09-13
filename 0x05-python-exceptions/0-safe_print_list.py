35a96b2b3947914fd75e630899068630e47b9f7f
CheropS <sh.cherop@gmail.com>
2022-09-13 12:40:06 +0000

    rectfying error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    """Print x elements of a list.
-    Args:
-        my_list (list): original list.
-        x (int): number of elements to print.
-    Returns:
-        Number of elements to be printed.
-    """
-    ret = 0
-    for j in range(x):
-        try:
-            print("{}".format(my_list[j], end="")
-            ret += 1
-        except IndexError:
-            break
-    print("")
-    return (ret)
+    ptr = 0
+
+    try:
+        for i in my_list:
+            if ptr < x:
+                print('{}'.format(my_list[ptr]), end='')
+                ptr += 1
+
+        print()
+    except TypeError:
+        pass
+    finally:
+        return ptr
