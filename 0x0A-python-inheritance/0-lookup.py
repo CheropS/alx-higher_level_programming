0820e4ab6045e4b50db8e1054b9186bc57575ede
CheropS <sh.cherop@gmail.com>
2022-09-26 17:00:01 +0000

    first commit

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+"""returns list of available attributes"""
+
+def lookup(obj):
+    """returns a list object"""
+    return (dir(obj))
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+The folder contains inheritance and its functions
