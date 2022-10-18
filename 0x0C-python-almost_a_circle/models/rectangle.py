589cdda10e32181af0e40b412cb784932d808d49
CheropS <sh.cherop@gmail.com>
2022-10-18 14:09:36 +0000

    add display message

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -84,10 +84,16 @@ class Rectangle(Base):
         return self.width * self.height
 
     def display(self):
-        """prints out in stdout instance with the character #"""
+        """prints out in stdout instance with the '#' character."""
         if self.width == 0 or self.height == 0:
             print("")
             return
+
+        [print("") for y in range(self.y)]
+        for h in range(self.height):
+            [print(" ", end="") for x in range(self.x)]
+            [print("#", end="") for w in range(self.width)]
+            print("")
 
     def update(self, *args, **kwargs):
         """Update the Rectangle.
