98cb18d94641c6a508dd1a354952f8aab0a260c2
CheropS <sh.cherop@gmail.com>
2023-02-17 18:13:37 +0000

    updated scripts

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
-# Lists all State objects from the database hbtn_0e_6_usa.
-# Usage: ./7-model_state_fetch_all.py <mysql username> /
-#                                     <mysql password> /
-#                                     <database name>
+"""
+    script that lists all State objects from the database hbtn_0e_6_usa
+"""
 import sys
-from sqlalchemy import create_engine
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from sqlalchemy import asc
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
 
-    for state in session.query(State).order_by(State.id):
-        print("{}: {}".format(state.id, state.name))
+    start = sessionmaker()
+    start.configure(bind=engine)
+    session = start()
+    stmt = session.query(State).order_by(asc(State.id)).all()
+
+    for i in stmt:
+        print("{:d}: {:s}".format(i.id, i.name))
+
+    session.close()
--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
-# Prints the first State object from the database hbtn_0e_6_usa.
-# Usage: ./8-model_state_fetch_first.py <mysql username> /
-#                                       <mysql password> /
-#                                       <database name>
+"""
+    script that prints the first State object from the database hbtn_0e_6_usa
+"""
 import sys
-from sqlalchemy import create_engine
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from sqlalchemy import asc
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    state = session.query(State).order_by(State.id).first()
-    if state is None:
-        print("Nothing")
+    start = sessionmaker()
+    start.configure(bind=engine)
+    session = start()
+    stmt = session.query(State).order_by(asc(State.id)).first()
+    if stmt:
+        print("{:d}: {:s}".format(stmt.id, stmt.name))
     else:
-        print("{}: {}".format(state.id, state.name))
+        print("Nothing")
+    session.close()
